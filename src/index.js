7c01e4bc66c3199117d6a508d61726244a2aaf39
Luke Gill <bagrad007@gmail.com>
2021-04-19 10:46:32 -0400

    commit tree 16a840e06db07f74bda7c20e1a36515c0ad9ba0b

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+function fetchDogs() {
+    // To pass the tests, don't forget to return your fetch!
+    return fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => json);
+
+}
